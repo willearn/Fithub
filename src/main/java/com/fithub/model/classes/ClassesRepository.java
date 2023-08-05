6e91aa44182ed406e00a1b2a4d4725e334018e0d
ChrislaFolia <frank08ta@gmail.com>
2023-08-05 23:45:42 +0800

    Add DAOs about course

---
+++ src/main/java/com/fithub/model/classes/ClassesRepository.java
@@ -0,0 +1,7 @@
+package com.fithub.model.classes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassesRepository extends JpaRepository<Classes, Integer> {
+
+}
---
+++ src/main/java/com/fithub/model/classroom/ClassroomRepository.java
@@ -0,0 +1,8 @@
+package com.fithub.model.classroom;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassroomRepository extends JpaRepository<Classroom
+, Integer> {
+
+}
