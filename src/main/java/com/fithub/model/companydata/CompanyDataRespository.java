b0545e940ead0504ed2221612b5cecd47cdfbf6a
william <cc2260112@gmail.com>
2023-08-21 19:15:58 +0800

    刪除companydata

--- src/main/java/com/fithub/model/companydata/CompanyDataRespository.java
+++
@@ -1,7 +0,0 @@
-package com.fithub.model.companydata;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyDataRespository extends JpaRepository<CompanyData, Integer> {
-
-}
