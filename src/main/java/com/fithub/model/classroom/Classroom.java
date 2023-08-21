6c82f109a4944f2a7195ac48d33d0742176f6553
william <cc2260112@gmail.com>
2023-08-21 19:28:58 +0800

    修改classroom bean

--- src/main/java/com/fithub/model/classroom/Classroom.java
+++ src/main/java/com/fithub/model/classroom/Classroom.java
@@ -24,14 +24,82 @@ public class Classroom {
 	@Column(name="classroomid")
 	private int classroomId;
 	
-	@Column(name="classroomlocation")
-	private String classroomLocation;
+	@Column(name="classroomname")
+	private String classroomname;
 	
 	@Column(name="classroomcapacity")
 	private int classroomCapacity;
 	
+	@Column(name="classroomdescription")
+	private String classroomdescription;
+	
+	@Column(name="classroompirce")
+	private int classroompirce;
+	
+	@Column(name="classroomstatus")
+	private String classroomstatus;
+
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "classroom")
 	private Set<Classes> classes=new HashSet<Classes>();
+
+	public Classroom() {
+	}
+
+	public int getClassroomId() {
+		return classroomId;
+	}
+
+	public void setClassroomId(int classroomId) {
+		this.classroomId = classroomId;
+	}
+
+	public String getClassroomname() {
+		return classroomname;
+	}
+
+	public void setClassroomname(String classroomname) {
+		this.classroomname = classroomname;
+	}
+
+	public int getClassroomCapacity() {
+		return classroomCapacity;
+	}
+
+	public void setClassroomCapacity(int classroomCapacity) {
+		this.classroomCapacity = classroomCapacity;
+	}
+
+	public String getClassroomdescription() {
+		return classroomdescription;
+	}
+
+	public void setClassroomdescription(String classroomdescription) {
+		this.classroomdescription = classroomdescription;
+	}
+
+	public int getClassroompirce() {
+		return classroompirce;
+	}
+
+	public void setClassroompirce(int classroompirce) {
+		this.classroompirce = classroompirce;
+	}
+
+	public String getClassroomstatus() {
+		return classroomstatus;
+	}
+
+	public void setClassroomstatus(String classroomstatus) {
+		this.classroomstatus = classroomstatus;
+	}
+
+	public Set<Classes> getClasses() {
+		return classes;
+	}
+
+	public void setClasses(Set<Classes> classes) {
+		this.classes = classes;
+	}
 	
 	
 }
