8ef344aa6286730fd49cf94ff29c92e4dbe3afe7
CR4-09\iSpan <frank08ta@gmail.com>
2023-08-23 11:12:00 +0800

    Entity Classes was modified with the DB Table rebuilt

--- src/main/java/com/fithub/model/classes/Classes.java
+++ src/main/java/com/fithub/model/classes/Classes.java
@@ -1,8 +1,19 @@
 package com.fithub.model.classes;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fithub.model.cart.Cart;
 import com.fithub.model.classroom.Classroom;
 import com.fithub.model.course.Course;
+import com.fithub.model.employee.Employee;
+import com.fithub.model.order.Order;
+import com.fithub.model.wishlist.Wishlist;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,7 +21,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -30,13 +44,13 @@ public class Classes {
 	@Column(name="classdate")
 	private String classDate;
 
-	@Column(name="classtimesince")
-	private String classTimeSince;
+	@Column(name="classtime")
+	private String classTime;
 
-	@Column(name="classtimeend")
-	private String classTimeEnd;
+	@Column(name="coachsubstitute")
+	private int coachSubstitute;
 
-	@Column(name="employeeid")
+	@Column(name="EMPLOYEEID")
 	private int employeeId;
 
 	@Column(name="applicantsceil")
@@ -57,8 +71,27 @@ public class Classes {
 	private Course course;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="EMPLOYEEID",insertable = false,updatable = false)
+	private Employee epmloyee;
+
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="CLASSROOMID",insertable = false,updatable = false)
 	private Classroom classroom;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "classes")
+	private Set<Wishlist> wishlist=new HashSet<Wishlist>();
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "classes")
+	private Set<Cart> cart=new HashSet<Cart>();
+
+//	@OneToMany(fetch = FetchType.LAZY, mappedBy = "classes")
+//	private Set<ClassesSet> classesSet=new HashSet<ClassesSet>();
+	
+	@JsonIgnore
+	@ManyToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	@JoinTable(name = "OrderItem", joinColumns = 
+	@JoinColumn(name = "classid"), inverseJoinColumns = @JoinColumn(name = "orderid"))
+	private List<Order> order = new ArrayList<Order>();
 	
 	
 }
