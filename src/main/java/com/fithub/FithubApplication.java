b238baf845c88afda03a236d3faf80de405a76b3
william <cc2260112@gmail.com>
2023-08-29 00:34:21 +0800

    新增JavaMail

--- src/main/java/com/fithub/FithubApplication.java
+++ src/main/java/com/fithub/FithubApplication.java
@@ -3,11 +3,16 @@ package com.fithub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fithub.util.JavaMail;
+
 @SpringBootApplication
 public class FithubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FithubApplication.class, args);
+		
+		JavaMail mail = new JavaMail();
+		mail.SendMail();
 	}
 
 }
---
+++ src/main/java/com/fithub/util/JavaMail.java
@@ -0,0 +1,70 @@
+package com.fithub.util;
+
+import java.util.Properties;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMessage.RecipientType;
+
+public class JavaMail {
+
+	private String username = "cc2260112@gmail.com";
+	private String password = "";
+	private String customer = "cc2260112@gmail.com";
+	private String subject = "測試";
+	private String txt = "信件內容文字";
+
+	public void SendMail() {
+		Properties prop = new Properties();
+		prop.setProperty("mail.transport.protocol", "smtp");
+		prop.setProperty("mail.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", 465);
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		prop.put("mail.smtp.socketFactory.port", "465");
+		//顯示連線資訊
+		prop.put("mail.debug","true");
+
+		Session session = Session.getDefaultInstance(prop, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		
+		MimeMessage message = new MimeMessage(session);
+		
+		
+		try {
+			//寄件者
+			message.setSender(new InternetAddress(username));
+			
+			//收件者
+			message.setRecipient(RecipientType.TO, new InternetAddress(customer));
+			
+			//標題
+			message.setSubject(subject);
+			
+			//內容
+			message.setContent(txt, "text/html;charset=UTF-8");
+			
+			//將message傳出
+			Transport transport = session.getTransport();
+			transport.send(message);
+			transport.close();
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
