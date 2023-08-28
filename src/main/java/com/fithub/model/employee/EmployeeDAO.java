77adb293471f5afc6e88f7d6d35560b051eebbe7
ChrislaFolia <frank08ta@gmail.com>
2023-08-29 00:52:36 +0800

    Add findEmployeeByEmployeetitle in EmployeeDao

--- src/main/java/com/fithub/model/employee/EmployeeDAO.java
+++ src/main/java/com/fithub/model/employee/EmployeeDAO.java
@@ -1,8 +1,15 @@
 package com.fithub.model.employee;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployeeDAO extends JpaRepository<Employee, Integer> {
 
+	@Query("from Employee where employeetitle = :employeetitle")
+	public List<Employee> findEmployeeByEmployeetitle(@Param(value="employeetitle") String employeetitle);
+	
 	
 }
