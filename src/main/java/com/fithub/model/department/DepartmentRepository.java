caed1b6930d2c7f8c3b3a3dd90639d38244f4ccd
CR4-10\iSpan <TW3555488@gmail.com>
2023-09-11 16:36:27 +0800

    部門新增分頁 查詢

--- src/main/java/com/fithub/model/department/DepartmentRepository.java
+++ src/main/java/com/fithub/model/department/DepartmentRepository.java
@@ -1,12 +1,23 @@
 package com.fithub.model.department;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-	
+	//XiaoQing
 	@Query("from Department where deptname = :name")
 	Department findDepartmentByName(@Param("name") String name);
+
+	//XiaoQing
+	@Query("select count(*) from Department where :name is null or deptname like %:name%")
+	long count(@Param("name")String name);
+
+	
+	//XiaoQing
+	@Query("select deptid , deptname from Department where :name is null or deptname like %:name%")
+	Page<Object[]> searchByName(Pageable pgb,@Param("name") String name);
 
 }
--- src/main/java/com/fithub/model/department/DepartmentService.java
+++ src/main/java/com/fithub/model/department/DepartmentService.java
@@ -4,7 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import com.fithub.model.backstageaccount.BackStageAccount;
 
 @Service
 public class DepartmentService implements IDepartmentService{
@@ -69,6 +75,34 @@ public class DepartmentService implements IDepartmentService{
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public long count(String name) {
+		long count = dRepo.count(name);
+		return count;
+	}
+
+	@Override
+	public long count() {
+		long count = dRepo.count();
+		return count;
+	}
+
+	@Override
+	public Page<Object[]> findPageByName(Integer pageNumber, Integer rows, String name) {
+		Pageable pgb = PageRequest.of(pageNumber, rows, Sort.DEFAULT_DIRECTION, "deptid");
+		Page<Object[]> page = dRepo.searchByName(pgb, name);
+		return page;
+	}
+
+	@Override
+	public Page<Department> findByPage(Integer pageNumber, Integer rows) {
+		Pageable pgb = PageRequest.of(pageNumber, rows, Sort.DEFAULT_DIRECTION, "deptid");
+
+		Page<Department> page = dRepo.findAll(pgb);
+
+		return page;
 	}
 	
 
