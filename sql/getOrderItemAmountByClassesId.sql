ed776218235f6ff36682c2ed7ed39da924565f61
CR4-09\iSpan <frank08ta@gmail.com>
2023-09-18 17:43:25 +0800

    modify sql file for orderitem amount

--- sql/getOrderItemAmountByClassesId.sql
+++ sql/getOrderItemAmountByClassesId.sql
@@ -1,7 +1,7 @@
 USE FithubTest
 GO
 
-SELECT cl.classid, count(oi.orderid) orderAmount
+SELECT oi.classid, count(oi.orderid) orderAmount
   FROM OrderItem oi JOIN Orders o ON oi.orderid = o.orderid 
   WHERE oi.classid=22
   GROUP BY oi.classid;
