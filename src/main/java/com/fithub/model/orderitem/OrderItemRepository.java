2292b5a7fef550caa882be8d358acdd16f335a36
CR4-09\iSpan <frank08ta@gmail.com>
2023-09-18 17:41:54 +0800

    Create @GetMapping(/getOrderItemAmountByClassId) in Orderitem controller

--- src/main/java/com/fithub/model/orderitem/OrderItemRepository.java
+++ src/main/java/com/fithub/model/orderitem/OrderItemRepository.java
@@ -1,13 +1,12 @@
 package com.fithub.model.orderitem;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import com.fithub.model.order.Order;
 
 import jakarta.transaction.Transactional;
 
@@ -19,4 +18,9 @@ public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
 	@Modifying
 	@Query("from OrderItem where orderId = :orderid")
 	List<OrderItem> getAllOrderItemByOrderId(@Param("orderid") Integer orderid);
+
+	// Chrislafolia，返回在指定classId的課程購買人數
+	@Query("SELECT oi.classId classId, count(oi.orderId) orderAmount "
+			+ "FROM OrderItem oi WHERE  oi.classId = :classId GROUP BY oi.classId")
+	Map<String, Object> getOrderItemAmountByClassId(@Param("classId") int classId);
 }
